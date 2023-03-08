21b5afda47be350f980c2e2b76dda359f9ce00e9
larguesa <rlarguesa@gmail.com>
2023-03-08 20:40:42 -0300

    Adicionado me component

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,19 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MeComponent } from './me/me.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MeComponent],
   template: `
     <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <app-me></app-me>
   `,
 })
 export class App {
-  name = 'Tópicos Especiais II';
+  name = 'TE II';
 }
 
 bootstrapApplication(App);
