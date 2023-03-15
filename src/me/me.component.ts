0df3dbd75224b279ead0c62d255c49c6280467c1
larguesa <rlarguesa@gmail.com>
2023-03-15 19:58:06 -0300

    1 file modified

--- src/me/me.component.ts
+++ src/me/me.component.ts
@@ -12,16 +12,6 @@ import { FormsModule } from '@angular/forms';
 export class MeComponent implements OnInit {
   myName: string;
 
-  n = 10;
-
-  getNumbers() {
-    let arr = [];
-    for (let i = 1; i <= this.n; i++) {
-      arr.push(i);
-    }
-    return arr;
-  }
-
   constructor() {
     this.myName = 'Ricardo';
   }
